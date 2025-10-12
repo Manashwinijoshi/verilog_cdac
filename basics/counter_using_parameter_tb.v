2af3e07ddc7bb2ad4956c5ec16dd77fade3a1ea1
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2025-10-12 15:20:53 +0530

    Update counter_using_parameter_tb.v

--- basics/counter_using_parameter_tb.v
+++ basics/counter_using_parameter_tb.v
@@ -17,6 +17,33 @@ module test;
     @(negedge clk) reset=1;
     @(negedge clk) reset=0; 
     #100;
+        
+  end
+endmodule
+
+//use of the defparam to change the values of the parameter
+// Code your testbench here
+// or browse Examples
+module test;
+  
+  reg clk,reset;
+  wire [3-1:0]out;
+  //using defparam to change the parameter values
+  defparam dut.N=3;
+  defparam dut.up_down=0;
+  
+  counter_up_down#(3,0) dut (clk,reset,out);
+  
+  initial clk=0;
+  always #5 clk=~clk;
+  initial begin
+    $monitor("reset=%b  out=%d ",reset,out);
+    clk=0;
+    reset=0;
+    
+    @(negedge clk) reset=1;
+    @(negedge clk) reset=0; 
+    #100;
  
     
     
